b029e9d8bbb74f1dbeda8485e55d38e4730b34ea
SosukeAizen833 <Spandan833@gmai.com>
2022-01-17 15:20:26 +0530

    form created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+import Header from './components/Header';
+import "./App.css"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      tasks: [],
+    }
+    this.addTask = this.addTask.bind(this)
+
+
+  }
+  addTask(task) {
+    this.setState({ tasks: this.state.tasks.push(task) });
+  }
+  render(){
+    return (
+
+        <div>
+          <Header/>
+          <form className='col-sm-4 mt-4 addTask container'>
+            <div className='row'>
+            <div className="form-group">
+              <label for="name" >Name</label>
+              <input type="text" className="form-control" placeholder="Homework"id="name"/>
+            </div>
+            <div className="form-group">
+              <label for="description">Description</label>
+              <textarea className="form-control" id="description"/>
+            </div>
+            <div className="form-group">
+              <label for="priority">Priority</label>
+              <select className="form-control" id="priority">
+                <option value="1">High</option>
+                <option value="2">Mid</option>
+                <option value="3">Low</option>
+              </select>
+            </div>
+            <button type="submit" style={{float: 'right', display: 'block',marginLeft: 'auto'}} className="btn btn-primary col-3 mt-4">Submit</button>
+            </div>
+          </form>
+
+        </div>
+    )
+  }
+
+  
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,8 @@
+import  React, { Component} from 'react';
+
+class Overview extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+}
