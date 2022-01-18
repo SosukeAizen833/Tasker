53c732d76b3cf50420b1096d601403a1b594b29c
SosukeAizen833 <Spandan833@gmai.com>
2022-01-18 18:07:46 +0530

    Task Overview Added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
 
 import React,{Component} from 'react';
 import Header from './components/Header';
+import Overview from './components/Overview';
+import uniqid from 'uniqid';
 import "./App.css"
 class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      tasks: [],
+      tasks: []
     }
     this.addTask = this.addTask.bind(this)
-
-
+  
   }
-  addTask(task) {
-    this.setState({ tasks: this.state.tasks.push(task) });
+  addTask(event) {
+
+    let name = event.target.name.value;
+    let description = event.target.description.value;
+    let priority = event.target.priority.value;
+    let id = uniqid();
+    let task = {name, description, priority,id}
+    let tasks = this.state.tasks
+    this.setState({ tasks: [...tasks, task] },() => {console.log(tasks)})
+    
+    event.preventDefault()
+  
+    
+    
   }
+  
+  
   render(){
     return (
 
         <div>
           <Header/>
-          <form className='col-sm-4 mt-4 addTask container'>
+          <div style={{display: 'flex', marginTop: '10px'}}>
+          <form className='col-sm-4 addTask container' onSubmit={this.addTask}>
             <div className='row'>
             <div className="form-group">
-              <label for="name" >Name</label>
-              <input type="text" className="form-control" placeholder="Homework"id="name"/>
+              <label htmlFor="name" >Name</label>
+              <input type="text"className="form-control" placeholder="Homework"id="name"/>
             </div>
             <div className="form-group">
-              <label for="description">Description</label>
-              <textarea className="form-control" id="description"/>
+              <label htmlFor="description">Description</label>
+              <textarea className="form-control"  id="description"/>
             </div>
             <div className="form-group">
-              <label for="priority">Priority</label>
+              <label htmlFor="priority">Priority</label>
               <select className="form-control" id="priority">
                 <option value="1">High</option>
                 <option value="2">Mid</option>
@@ -42,7 +58,9 @@ class App extends Component {
             <button type="submit" style={{float: 'right', display: 'block',marginLeft: 'auto'}} className="btn btn-primary col-3 mt-4">Submit</button>
             </div>
           </form>
-
+          <Overview tasks={this.state.tasks}/>
+        
+          </div>
         </div>
     )
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,8 +1,63 @@
 import  React, { Component} from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-class Overview extends Component {
+export default class Overview extends Component {
     constructor(props) {
         super(props);
+        
+        this.renderTasks = this.renderTasks.bind(this);
     }
+    renderTasks(tasks){
+            let taskArray = [];
+            let tableRowStyle = {display: 'flex',justifyContent:"space-evenly",alignItems: 'center',backgroundColor: "orange",padding:"10px 6px"}
+            for(let task of tasks){
+                const element =(
+                <div>
+                    <Accordion style={{backgroundColor: "#DE9D90"}} key={task.id}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header">
+                        <Typography>{task.name}</Typography>
+                        <Typography style={{marginLeft:"auto"}}>{task.priority}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                {task.description}
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                            
+                </div>
+                                
+                )
+                
+                taskArray.push(element);
+            }
+
+            return taskArray;
+      
+    }
+    render(){
+        let taskTable = {
+            display: 'flex',
+            flexDirection: 'column',
+            gap: "10px",
+            width:"70%",
+            padding: "10px"
+        }
+        return(
+            <div style={taskTable}>
+                {this.renderTasks(this.props.tasks)}
+            </div>
     
+        )
+    }
 }
+
+
+
